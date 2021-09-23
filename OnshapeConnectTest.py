1d159670d0614d111d2cc0ba1b815bb9b6789db9
Matt Mueller <54808875+matthewlouismueller@users.noreply.github.com>
2021-09-23 11:12:46 -0400

    back

--- OnshapeConnectTest.py
+++ OnshapeConnectTest.py
@@ -1,31 +1,7 @@
 from OnshapePlus import *
 
 try:
-    try:
-        exec(open('../apikeys.py').read())
-        base = 'https://cad.onshape.com'
-        client = Client(configuration={"base_url": base,
-                                    "access_key": access,
-                                    "secret_key": secret})
-        print('client configured')
-    except:
-        exec(open('apikeys.py').read())
-        base = 'https://cad.onshape.com'
-        client = Client(configuration={"base_url": base,
-                                    "access_key": access,
-                                    "secret_key": secret})
-        print('client configured')
-except:
-    print('Api keys file not found. Input keys manually.')
-    access = input('Please enter your access key: ')
-    secret = input('Please enter your secret key: ')
-    base = 'https://cad.onshape.com'
-    client = Client(configuration={"base_url": base,
-                                "access_key": access,
-                                "secret_key": secret})
-    print('client configured')
-
-try:
-    print(documents(client))
+    print(documents())
 except:
     print('fail')
+
--- OnshapePlus.py
+++ OnshapePlus.py
@@ -14,6 +14,31 @@ import json
 from onshape_client.client import Client
 from onshape_client.onshape_url import OnshapeElement
 
+try:
+    try:
+        exec(open('../apikeys.py').read())
+        base = 'https://cad.onshape.com'
+        client = Client(configuration={"base_url": base,
+                                    "access_key": access,
+                                    "secret_key": secret})
+        print('client configured')
+    except:
+        exec(open('apikeys.py').read())
+        base = 'https://cad.onshape.com'
+        client = Client(configuration={"base_url": base,
+                                    "access_key": access,
+                                    "secret_key": secret})
+        print('client configured')
+except:
+    print('Api keys file not found. Input keys manually.')
+    access = input('Please enter your access key: ')
+    secret = input('Please enter your secret key: ')
+    base = 'https://cad.onshape.com'
+    client = Client(configuration={"base_url": base,
+                                "access_key": access,
+                                "secret_key": secret})
+    print('client configured')
+
 ##
 ##
 ## Define Serial Functions
@@ -119,7 +144,7 @@ def massProp(client, url: str, base):
   return parsed
 
 ## Basic Documents test
-def documents(client, params = {}):
+def documents(params = {}):
   fixed_url = '/api/documents'
   method = 'GET'
 
