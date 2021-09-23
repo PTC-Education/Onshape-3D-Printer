7787875c5ba983a9ed90ce6a9badd767255fb640
Matt Mueller <54808875+matthewlouismueller@users.noreply.github.com>
2021-09-23 11:08:12 -0400

    config in file

--- OnshapePlus.py
+++ OnshapePlus.py
@@ -13,31 +13,6 @@ import json
 ##
 from onshape_client.client import Client
 from onshape_client.onshape_url import OnshapeElement
-base = 'https://cad.onshape.com'
-try:
-    try:
-        exec(open('../apikeys.py').read())
-        base = 'https://cad.onshape.com'
-        client = Client(configuration={"base_url": base,
-                                    "access_key": access,
-                                    "secret_key": secret})
-        print('client configured')
-    except:
-        exec(open('apikeys.py').read())
-        base = 'https://cad.onshape.com'
-        client = Client(configuration={"base_url": base,
-                                    "access_key": access,
-                                    "secret_key": secret})
-        print('client configured')
-except:
-    print('Api keys file not found. Input keys manually.')
-    access = input('Please enter your access key: ')
-    secret = input('Please enter your secret key: ')
-    base = 'https://cad.onshape.com'
-    client = Client(configuration={"base_url": base,
-                                "access_key": access,
-                                "secret_key": secret})
-    print('client configured')
 
 ##
 ##
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Onshape-3D-Printer
+
+Start by cloning this repository, then going into the new folder
+
+```
+git clone https://github.com/PTC-Education/Onshape-3D-Printer
+cd Onshape-3D-Printer
+```
+
+Then running this command to install all packages
+
+```
+pip3 install -r requirements.txt
+```
