378796f9d75b586618572af9d22d69086a6b538c
Matt Mueller <54808875+matthewlouismueller@users.noreply.github.com>
2021-09-27 07:27:45 -0400

    logo choice

--- ExportSliceUpload.py
+++ ExportSliceUpload.py
@@ -5,12 +5,13 @@ from Octoprint import *
 url = input('Input url of Part Studio you want to export as STL: ')
 print('default configuration for token = "CoinText%3DMatt%3BList_Os3Ch7CJl5rv3f%3DPTC"')
 configurationText = input('Input the configuration text (just click enter if there are none): ')
+logoChoice = input('Do you want PTC or Onshape logo on the back?: ')
 filename = input('Input the filename for the stl (just click enter if you want to use "OnshapePart.stl"): ')
 printerConfig = input('Input the name of your 3D printer config file: ')
 # printerConfig = "colabConfigBundle.ini"
 
 try:
-    exportSTL(url,filename,'CoinText%3D'+configurationText)
+    exportSTL(url,filename,'CoinText%3D'+configurationText+'%3BList_Os3Ch7CJl5rv3f%3D'+logoChoice)
     command = "slic3r-prusa3d --no-gui --load "+printerConfig+" "+filename
     # Suppress the output in order to not break the EMSSS
     suppress = " >/dev/null 2>&1"
