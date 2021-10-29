da8d821f66301b5f87bd9f6b45f39ab314ff0fe5
Matt Mueller <54808875+matthewlouismueller@users.noreply.github.com>
2021-10-28 20:07:39 -0400

    onshape control

--- AL5DOnshapeMateControl.py
+++ AL5DOnshapeMateControl.py
@@ -1,15 +1,6 @@
-from SerialSetup import *
 from AL5Dsetup import *
 
-# ports = serial_ports()
-# print(ports)
-# portname = input('Ports found listed above. Enter name of port for robot arm: ')
-# ser = serial.Serial(
-#     port=portname,
-#     baudrate=9600
-# )
-
-# print(ser.isOpen())
+url = input("Enter the URL of your Onshape Assembly: ")
 
 LastJointArray = [1500,1500,1500,1500,1500,1500]
 
@@ -17,18 +8,18 @@ smoothingThreshold = 50
 steps = 15
 delay = 0.05
 
-mateValuesNow, mateResponse = getMateValues()
+mateValuesNow, mateResponse = getMateValues(url)
 
 while True:
     ndPosArray = [0 for i in range(6)]
     empty = [0 for i in range(6)]
     ndPosArray = np.vstack([ndPosArray,empty])
     
-    checkpointPos = getCheckpointPos()
+    checkpointPos = getCheckpointPos(url)
     checkpointOffset = [-0.006,-0.01,0.007]
     newMateValues = GetJointAngles(checkpointPos[0]+checkpointOffset[0],checkpointPos[1]+checkpointOffset[1],checkpointPos[2]+checkpointOffset[2],mateValuesNow[6])
     print(mateValuesNow[7],mateValuesNow[8])
-    setMateValues(mateResponse,newMateValues[0],newMateValues[1],newMateValues[2],newMateValues[3],mateValuesNow[7],mateValuesNow[8])
+    setMateValues(url,mateResponse,newMateValues[0],newMateValues[1],newMateValues[2],newMateValues[3],mateValuesNow[7],mateValuesNow[8])
     mateValuesNow, mateResponse = getMateValues()
 
     for i in range(6):
@@ -45,10 +36,5 @@ while True:
 
     for x in ndPosArray:
         print(x)
-        # for i in range(len(x)):
-        #     if x[i] != 0:
-        #         command = '#'+str(i)+' P'+str(x[i])+'\r'
-        #         ser.write(command.encode())
-        #         time.sleep(delay)
     LastJointArray = mateValuesNow[0:5]
     time.sleep(0.5)
